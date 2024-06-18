fd4cd7b2d8766f4e63aa7b4f65641d49249095e8
Gordi42 <silvano98@hotmail.de>
2024-06-18 16:37:37 +0200

    change doc theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -28,5 +28,5 @@ pygments_style = 'sphinx'
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'default'
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
