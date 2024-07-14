e91d495fedfbb7ca6fc5d3b909c4477264c692f5
Gordi42 <silvano98@hotmail.de>
2024-07-14 03:12:28 +0200

    add lazy loading to projection module

--- src/fridom/framework/projection/__init__.py
+++ src/fridom/framework/projection/__init__.py
@@ -32,15 +32,82 @@ Nonlinear Projections
 `NNMDBase`
     Balancing using nonlinear normal mode decomposition.
 """
-# base projection class
-from .projection import Projection
+import sys
+from types import ModuleType
+import importlib
+from typing import TYPE_CHECKING
 
-# linear projections
-from .wave_spectral import WaveSpectralBase
-from .divergence_spectral import DivergenceSpectralBase
-from .geostrophic_spectral import GeostrophicSpectralBase
-from .geostrophic_time_average import GeostrophicTimeAverage
+# ================================================================
+#  Disable lazy loading for type checking
+# ================================================================
+if TYPE_CHECKING:
+    # base projection class
+    from .projection import Projection
 
-# nonlinear projections
-from .nnmd import NNMDBase
-from .optimal_balance import OptimalBalance
+    # linear projections
+    from .wave_spectral import WaveSpectralBase
+    from .divergence_spectral import DivergenceSpectralBase
+    from .geostrophic_spectral import GeostrophicSpectralBase
+    from .geostrophic_time_average import GeostrophicTimeAverage
+
+    # nonlinear projections
+    from .nnmd import NNMDBase
+    from .optimal_balance import OptimalBalance
+
+# ================================================================
+#  Define all the possible imports
+# ================================================================
+
+# Set up dictionary that maps an import to a path
+# items in the all_modules_by_origin dictionary are imported as modules
+all_modules_by_origin = { }
+
+# items in the all_imports_by_origin dictionary are imported as elements of a module
+base_path = "fridom.framework.projection"
+all_imports_by_origin = { 
+    f"{base_path}.projection": ["Projection"],
+    f"{base_path}.wave_spectral": ["WaveSpectralBase"],
+    f"{base_path}.divergence_spectral": ["DivergenceSpectralBase"],
+    f"{base_path}.geostrophic_spectral": ["GeostrophicSpectralBase"],
+    f"{base_path}.geostrophic_time_average": ["GeostrophicTimeAverage"],
+    f"{base_path}.nnmd": ["NNMDBase"],
+    f"{base_path}.optimal_balance": ["OptimalBalance"],
+}
+
+# ================================================================
+#  Set up the import system
+# ================================================================
+
+origins = {}
+_all_modules = []
+for origin, items in all_modules_by_origin.items():
+    for item in items:
+        _all_modules.append(item)
+        origins[item] = origin
+
+_all_imports = []
+for origin, items in all_imports_by_origin.items():
+    for item in items:
+        _all_imports.append(item)
+        origins[item] = origin
+
+# load submodules on demand
+class _module(ModuleType):
+    def __getattr__(self, name):
+        # check if the attribute is a module
+        if name in _all_modules:
+            res = importlib.import_module(origins[name] + "." + name)
+        # check if the attribute is an import
+        elif name in _all_imports:
+            mod = importlib.import_module(origins[name])
+            res = getattr(mod, name)
+        # if the attribute is not found
+        else:
+            raise AttributeError(f"module {__name__!r} has no attribute {name!r}")
+        # set the attribute in the current module such that it is not loaded again
+        setattr(self, name, res)
+        # return the attribute
+        return res
+
+sys.modules[__name__].__class__ = _module
+__all__ = _all_modules + _all_imports
