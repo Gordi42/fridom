2b2d05a5af0ca8f1008e055ed69033e43a77cd69
Gordi42 <silvano98@hotmail.de>
2024-07-14 02:55:30 +0200

    lazy loading for nonhydro grid

--- src/fridom/framework/grid/__init__.py
+++ src/fridom/framework/grid/__init__.py
@@ -13,8 +13,67 @@ Classes
 `CartesianGrid`
     A regular grid in Cartesian coordinates with constant grid spacing.
 """
-# import modules
-from . import cartesian
+import sys
+from types import ModuleType
+import importlib
+from typing import TYPE_CHECKING
 
-# import classes
-from .cartesian import CartesianGrid
+# ================================================================
+#  Disable lazy loading for type checking
+# ================================================================
+if TYPE_CHECKING:
+    # import modules
+    from . import cartesian
+
+    # import classes
+    from .cartesian import CartesianGrid
+    
+
+# ================================================================
+#  Define all the possible imports
+# ================================================================
+
+# Set up dictionary that maps an import to a path
+# items in the all_modules_by_origin dictionary are imported as modules
+all_modules_by_origin = { "fridom.nonhydro": ["cartesian"] }
+
+# items in the all_imports_by_origin dictionary are imported as elements of a module
+all_imports_by_origin = { "fridom.nonhydro.cartesian": ["CartesianGrid"] }
+
+# ================================================================
+#  Set up the import system
+# ================================================================
+
+origins = {}
+_all_modules = []
+for origin, items in all_modules_by_origin.items():
+    for item in items:
+        _all_modules.append(item)
+        origins[item] = origin
+
+_all_imports = []
+for origin, items in all_imports_by_origin.items():
+    for item in items:
+        _all_imports.append(item)
+        origins[item] = origin
+
+# load submodules on demand
+class _module(ModuleType):
+    def __getattr__(self, name):
+        # check if the attribute is a module
+        if name in _all_modules:
+            res = importlib.import_module(origins[name] + "." + name)
+        # check if the attribute is an import
+        elif name in _all_imports:
+            mod = importlib.import_module(origins[name])
+            res = getattr(mod, name)
+        # if the attribute is not found
+        else:
+            raise AttributeError(f"module {__name__!r} has no attribute {name!r}")
+        # set the attribute in the current module such that it is not loaded again
+        setattr(self, name, res)
+        # return the attribute
+        return res
+
+sys.modules[__name__].__class__ = _module
+__all__ = _all_modules + _all_imports
