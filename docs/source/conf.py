6a37ae9d930b0bf940f886e02563aa98ba1980ff
Gordi42 <silvano98@hotmail.de>
2024-07-31 17:29:07 +0200

    change back to rtd theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -49,8 +49,8 @@ pygments_style = 'sphinx'
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-# html_theme = 'sphinx_rtd_theme'
-html_theme = 'sphinxawesome_theme'
+html_theme = 'sphinx_rtd_theme'
+# html_theme = 'sphinxawesome_theme'
  
 
 html_static_path = ['_static']
