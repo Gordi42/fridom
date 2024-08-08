826b6c67398b4fbd71b7654b9878b5fb9920e4eb
Gordi42 <silvano98@hotmail.de>
2024-08-08 17:22:22 +0200

    updating advection schemes to new setup system

--- src/fridom/nonhydro/modules/advection/centered_advection.py
+++ src/fridom/nonhydro/modules/advection/centered_advection.py
@@ -1,20 +1,9 @@
 import fridom.framework as fr
 import fridom.nonhydro as nh
-# Import external modules
-from typing import TYPE_CHECKING
-from functools import partial
-# Import internal modules
-from fridom.framework import config, utils
-from fridom.framework.modules.module import Module, setup_module, module_method
-# Import type information
-if TYPE_CHECKING:
-    from fridom.framework.model_state import ModelState
-    from fridom.framework.grid import InterpolationModule, DiffBase
-    from fridom.framework.field_variable import FieldVariable
 
 
 @fr.utils.jaxify
-class CenteredAdvection(Module):
+class CenteredAdvection(fr.modules.Module):
     """
     Centered advection scheme.
 
@@ -41,36 +30,18 @@ class CenteredAdvection(Module):
         The interpolation module to use.
         If None, the interpolation module of the grid is used.
     """
-    def __init__(self, 
-                 diff: 'DiffBase | None' = None,
-                 interpolation: 'InterpolationModule | None' = None):
+    def __init__(self):
         super().__init__(name="Centered Advection")
-        self._diff = diff
-        self._interpolation = interpolation
         self.required_halo = 2
         return
 
-    @setup_module
-    def setup(self):
-        # setup the differentiation modules
-        if self.diff is None:
-            self.diff = self.mset.grid._diff_mod
-        else:
-            self.diff.setup(mset=self.mset)
-
-        # setup the interpolation modules
-        if self.interpolation is None:
-            self.interpolation = self.mset.grid._interp_mod
-        else:
-            self.interpolation.setup(mset=self.mset)
-        return
-
-    @utils.jaxjit
+    @fr.utils.jaxjit
     def flux_divergence(self, 
-                        velocity: 'tuple[FieldVariable]',
-                        quantity: 'FieldVariable') -> 'FieldVariable':
+                        velocity: 'tuple[fr.FieldVariable]',
+                        quantity: 'fr.FieldVariable') -> 'fr.FieldVariable':
         # shorthand notation
-        inter = self.interpolation.interpolate
+        inter = self.interp_module.interpolate
+        diff = self.diff_module.diff
         Ro = self.mset.Ro
         q_pos = quantity.position
 
@@ -80,10 +51,10 @@ class CenteredAdvection(Module):
             # the flux position should be shifted from the quantity position
             flux_pos = q_pos.shift(axis)
             flux = inter(v, flux_pos) * inter(quantity, flux_pos)
-            res += flux.diff(axis, order=1)
+            res += diff(flux, axis, order=1)
         return Ro * res
 
-    @utils.jaxjit
+    @fr.utils.jaxjit
     def advect_state(self, 
                      z: nh.State, 
                      dz: nh.State, 
@@ -96,8 +67,8 @@ class CenteredAdvection(Module):
         return dz
 
 
-    @module_method
-    def update(self, mz: 'ModelState') -> None:
+    @fr.modules.module_method
+    def update(self, mz: 'fr.ModelState') -> None:
         """
         Compute the advection term of the state vector z.
         """
@@ -109,33 +80,3 @@ class CenteredAdvection(Module):
         mz.dz = self.advect_state(zf, mz.dz, velocity)
 
         return mz
-
-    # ================================================================
-    #  Properties
-    # ================================================================
-    @property
-    def info(self) -> dict:
-        res = super().info
-        res["diff"] = self.diff
-        res["interpolation"] = self.interpolation
-        return res
-
-    @property
-    def diff(self) -> 'DiffBase':
-        """The differentiation module."""
-        return self._diff
-    
-    @diff.setter
-    def diff(self, value: 'DiffBase'):
-        self._diff = value
-        return
-
-    @property
-    def interpolation(self) -> 'InterpolationModule':
-        """The interpolation module."""
-        return self._interpolation
-
-    @interpolation.setter
-    def interpolation(self, value: 'InterpolationModule'):
-        self._interpolation = value
-        return
