e77514176d4b38b1dc9cc1bb8bbd5d50761bbdf7
Gordi42 <silvano98@hotmail.de>
2024-08-09 22:18:27 +0200

    move k2_hat array to spectral pressure solver

--- src/fridom/nonhydro/grid/cartesian/grid.py
+++ src/fridom/nonhydro/grid/cartesian/grid.py
@@ -1,9 +1,9 @@
 from numpy import ndarray
 import fridom.nonhydro as nh
 import fridom.framework as fr
-from functools import partial
 
-@partial(fr.utils.jaxify, dynamic=('k2_hat', 'k2_hat_zero'))
+
+@fr.utils.jaxify
 class Grid(fr.grid.cartesian.Grid):
     def __init__(self, N: list[int], L: list[int],
                  periodic_bounds: list[bool] = [True, True, True],
@@ -15,31 +15,6 @@ class Grid(fr.grid.cartesian.Grid):
         else:
             raise ValueError(f"Unknown decomposition {decomposition}")
         super().__init__(N, L, periodic_bounds, shared_axes)
-
-        self.k2_hat = None
-        self.k2_hat_zero = None
-
-    def setup(self, mset: nh.ModelSettings):
-        super().setup(mset)
-
-        # discretized wave number squared
-
-        from fridom.framework.grid.cartesian import discrete_spectral_operators as dso
-        k_dis = [dso.k_hat_squared(kx, dx, use_discrete=True)
-                 for (kx,dx) in zip(self.K, self.dx)]
-
-        # scaled discretized wave number squared
-        k2_hat = k_dis[0] + k_dis[1] + k_dis[2] / mset.dsqr
-
-        k2_hat_zero = fr.config.ncp.where(k2_hat == 0)
-        fr.utils.modify_array(k2_hat, k2_hat_zero, 1)
-
-        # ----------------------------------------------------------------
-        #  Set attributes
-        # ----------------------------------------------------------------
-        self.k2_hat = k2_hat
-        self.k2_hat_zero = k2_hat_zero
-        return
 
     def omega(self, 
               k: tuple[float] | tuple[ndarray],
--- src/fridom/nonhydro/modules/pressure_solvers/spectral_pressure_solver.py
+++ src/fridom/nonhydro/modules/pressure_solvers/spectral_pressure_solver.py
@@ -1,4 +1,6 @@
 import fridom.framework as fr
+import fridom.nonhydro as nh
+import numpy as np
 
 
 @fr.utils.jaxify
@@ -8,11 +10,32 @@ class SpectralPressureSolver(fr.modules.Module):
     """
     name = "Spectral Pressure Solver"
 
+    @fr.modules.module_method
+    def setup(self, mset: 'nh.ModelSettings') -> None:
+        super().setup(mset)
+        # check if the grid is cartesian
+        if not isinstance(mset.grid, fr.grid.cartesian.Grid):
+            raise ValueError("Spectral solver only works with Cartesian grids.")
+        
+        # compute the discretized wave number squared
+        from fridom.framework.grid.cartesian import discrete_spectral_operators as dso
+        k_dis = [dso.k_hat_squared(kx, dx, use_discrete=True)
+                 for (kx,dx) in zip(self.grid.K, self.grid.dx)]
+
+        # scaled discretized wave number squared
+        k2_hat = k_dis[0] + k_dis[1] + k_dis[2] / mset.dsqr
+
+        # Compute the inverse of the wave number squared
+        with np.errstate(divide='ignore', invalid='ignore'):
+            k2_hat_inv = 1 / k2_hat
+
+        # Set k2_hat_inv to zero where k2_hat is zero
+        self.k2_hat_inv = fr.config.ncp.where(k2_hat == 0, 0, k2_hat_inv)
+        return
+
     @fr.utils.jaxjit
     def solve_for_pressure(self, div: fr.FieldVariable) -> fr.FieldVariable:
-        ps = div.fft() / (-self.grid.k2_hat)
-        ps.arr = fr.utils.modify_array(ps.arr, self.grid.k2_hat_zero, 0)
-        return ps.fft()
+        return ( - div.fft() * self.k2_hat_inv).fft()
 
 
     @fr.modules.module_method
