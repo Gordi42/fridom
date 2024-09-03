8250bac1f5c95262b8884f480f55d7ee033eeb55
Gordi42 <silvano98@hotmail.de>
2024-09-03 15:50:58 +0200

    fix the kelvin wave initial condition (include staggered grid effects and the scaled model)

--- src/fridom/nonhydro/initial_conditions/kelvin_wave.py
+++ src/fridom/nonhydro/initial_conditions/kelvin_wave.py
@@ -4,6 +4,8 @@ class KelvinWave(nh.State):
     """
     Initial condition with a kelvin wave at the boundary.
 
+    TODO: Add some more details about the math, add proper gallery example.
+
     Description
     -----------
     Lets consider a Kelvin wave at the southern boundary of the domain with
@@ -63,51 +65,74 @@ class KelvinWave(nh.State):
     .. code-block:: python
 
         z  = nh.initial_conditions.kelvin_wave(mset, 'N', kh=1, kz=2)
-        z += nh.initial_conditions.kelvin_wave(mset, 'N', kh=1, kz=-2)
+        z += nh.initial_conditions.kelvin_wave(mset, 'N', kh=1, kz=-2, phase=np.pi)
     """
     def __init__(self, 
                  mset: nh.ModelSettings, 
                  side: str, 
                  kz: int, 
-                 kh: int, 
+                 k_parallel: int, 
                  phase: float = 0):
         super().__init__(mset, is_spectral=False)
-    
         ncp = nh.config.ncp
-        X, Y, Z = mset.grid.X
+
+        # convert the wavenumbers
         Lx, Ly, Lz = mset.grid.L
-        match side:
-            case 'N':
-                Lh = Lx; sign = 1;  Xn = Ly - Y; Xw = X
-            case 'S':
-                Lh = Lx; sign = -1; Xn = Y;      Xw = X
-            case 'E':
-                Lh = Ly; sign = -1; Xn = Lx - X; Xw = Y
-            case 'W':
-                Lh = Ly; sign = 1;  Xn = X;      Xw = Y
-
         kz = 2 * ncp.pi * kz / Lz
-        kh = 2 * ncp.pi * kh / Lh
-        om = ncp.sqrt((kh**2 * mset.N2) / (kh**2 + kz**2))
-
-        wave = ncp.exp(1j * (kh * Xw + kz * Z + phase)) * ncp.sign(kz)
-        wave *= ncp.exp(- mset.f0 * kh / om * Xn)
-
-        # polarizations
-        up = - kz
-        wp = kh
-        bp = 1j * sign * mset.N2 * kh / om
-
-        # set the fields
         if side == "N" or side == "S":
-            self.u.arr = (up * wave).real
+            L_parallel = Lx
         if side == "E" or side == "W":
-            self.v.arr = (up * wave).real
-        self.w.arr = (wp * wave).real
-        self.b.arr = (bp * wave).real
+            L_parallel = Ly
+        k_parallel = 2 * ncp.pi * k_parallel / L_parallel
+
+        # calculate the frequency
+        om = ncp.sqrt((k_parallel**2 * mset.N2) / 
+                      (kz**2 + mset.dsqr * k_parallel**2))
+
+        # calculate the polarizations
+        pol_u_normal = - kz
+        pol_w = k_parallel
+        pol_b = - 1j * mset.N2 * k_parallel / om
+
+        # define function to get the exponential decay and the wave pattern
+        def wave(x_parallel, x_normal, z):
+            wave = ncp.exp(1j * (k_parallel * x_parallel + kz * z + phase))
+            wave *= ncp.exp(- mset.f0 * k_parallel / om * x_normal)
+            return wave
+
+        if side == "N":
+            def get_wave(f: nh.FieldVariable):
+                x, y, z = f.get_mesh()
+                x_normal = Ly - y
+                x_parallel = Lx - x
+                return wave(x_parallel, x_normal, z)
+            self.u.arr = (- pol_u_normal * get_wave(self.u)).imag
+        elif side == "S":
+            def get_wave(f: nh.FieldVariable):
+                x, y, z = f.get_mesh()
+                x_normal = y
+                x_parallel = x
+                return wave(x_parallel, x_normal, z)
+            self.u.arr = (pol_u_normal * get_wave(self.u)).imag
+        elif side == "E":
+            def get_wave(f: nh.FieldVariable):
+                x, y, z = f.get_mesh()
+                x_normal = Lx - x
+                x_parallel = y
+                return wave(x_parallel, x_normal, z)
+            self.v.arr = (pol_u_normal * get_wave(self.v)).imag
+        elif side == "W":
+            def get_wave(f: nh.FieldVariable):
+                x, y, z = f.get_mesh()
+                x_normal = x
+                x_parallel = Ly - y
+                return wave(x_parallel, x_normal, z)
+            self.v.arr = (- pol_u_normal * get_wave(self.v)).imag
+
+        self.w.arr = (pol_w * get_wave(self.w)).imag
+        self.b.arr = (pol_b * get_wave(self.b)).imag
 
         # save some parameters
-        self.kh = kh
+        self.k_parallel = k_parallel
         self.kz = kz
         self.om = om
-        return
