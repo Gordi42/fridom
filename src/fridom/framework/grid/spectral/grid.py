b58ece98e7a8539ab116400b309f96fa8c15532f
Gordi42 <silvano98@hotmail.de>
2024-09-11 18:08:49 +0200

    adding structure for padding in the spectral grid

--- src/fridom/framework/grid/spectral/grid.py
+++ src/fridom/framework/grid/spectral/grid.py
@@ -14,10 +14,15 @@ class Grid(fr.grid.cartesian.Grid):
                          diff_mod=fr.grid.spectral.SpectralDiff(),
                          interp_mod=fr.grid.InterpolationModule())
         self.name = "Spectral Grid"
+        self.mpi_available = False
 
     def setup(self, mset: 'fr.ModelSettingsBase'):
         fft = fr.grid.spectral.FFT(self._periodic_bounds)
         super().setup(mset, req_halo=0, fft_module=fft)
+
+        self.pad_extend_inner: tuple[slice] = None
+        self.pad_trim_inner: tuple[slice] = None
+        self.pad_extend: tuple[tuple[int]] = None
 
     def get_mesh(self, 
                  position: fr.grid.Position | None = None,
@@ -27,15 +32,33 @@ class Grid(fr.grid.cartesian.Grid):
     @partial(fr.utils.jaxjit, static_argnames=["transform_types"])
     def fft(self, 
             arr: np.ndarray,
-            transform_types: 'tuple[fr.grid.TransformType] | None' = None
+            transform_types: 'tuple[fr.grid.TransformType] | None' = None,
+            padding = fr.grid.FFTPadding.NOPADDING,
             ) -> np.ndarray:
+        # Forward transform the array
         f = lambda x, axes: self._fft.forward(x, axes, transform_types)
-        return self._pfft.forward_apply(arr, f)
+        u_hat = self._pfft.forward_apply(arr, f)
+        
+        # Apply padding if necessary
+        if padding == fr.grid.FFTPadding.EXTEND:
+            u_hat = u_hat[self.pad_extend_inner]
+        return u_hat
 
     @partial(fr.utils.jaxjit, static_argnames=["transform_types"])
     def ifft(self, 
              arr: np.ndarray,
              transform_types: 'tuple[fr.grid.TransformType] | None' = None
              ) -> np.ndarray:
+        ncp = fr.config.ncp
+        # Apply padding if necessary
+        match self._padding:
+            case fr.grid.FFTPadding.NOPADDING:
+                u = arr
+            case fr.grid.FFTPadding.TRIM:
+                u = ncp.zeros_like(arr)
+                u = fr.utils.modify_array(u, self.pad_trim_inner, arr)
+            case fr.grid.FFTPadding.EXTEND:
+                u = ncp.pad(arr, self.pad_extend, mode='constant')
+
         f = lambda x, axes: self._fft.backward(x, axes, transform_types)
-        return self._pfft.backward_apply(arr, f)
+        return self._pfft.backward_apply(u, f)
