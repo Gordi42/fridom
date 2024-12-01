ec16a5065e8d2c12c82ad6d284dc46e5eff3ba23
Gordi42 <silvano98@hotmail.de>
2024-12-01 13:52:46 +0100

    add new get_default_decomposition() method to the project tree

--- src/fridom/framework/domain_decomposition/__init__.py
+++ src/fridom/framework/domain_decomposition/__init__.py
@@ -10,7 +10,7 @@ from typing import TYPE_CHECKING
 #  Disable lazy loading for type checking
 # ================================================================
 if TYPE_CHECKING:
-    from .domain_decomposition import DomainDecomposition, get_domain_decomposition
+    from .domain_decomposition import DomainDecomposition, get_default_domain_decomposition
     from .single_decomposition import SingleDecomposition
     from .jax_decomposition import JaxDecomposition
 
@@ -22,7 +22,7 @@ all_modules_by_origin = { }
 dom_path = "fridom.framework.domain_decomposition"
 all_imports_by_origin = { 
     f"{dom_path}.domain_decomposition": ["DomainDecomposition", 
-                                         "get_domain_decomposition"], 
+                                         "get_default_domain_decomposition"], 
     f"{dom_path}.single_decomposition": ["SingleDecomposition"],
     f"{dom_path}.jax_decomposition": ["JaxDecomposition"],
 }
