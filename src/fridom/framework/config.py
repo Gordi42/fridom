422d9494e6d595f41ccc5426596fcbbc3d3e48df
Gordi42 <silvano98@hotmail.de>
2024-12-01 11:46:21 +0100

    adding a config flag to enable parallelization

--- src/fridom/framework/config.py
+++ src/fridom/framework/config.py
@@ -212,6 +212,7 @@ class Backend(Enum):
 backend: Backend = None
 backend_is_jax: bool
 enable_jax_jit: bool = True
+enable_parallel: bool = False  # enable parallel computation
 jax_jit_was_called: bool = False
 ncp = None  # numpy or cupy
 scp = None  # scipy or cupyx.scipy
