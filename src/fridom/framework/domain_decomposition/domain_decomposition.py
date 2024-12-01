19b1c33dc74bfc8cabcc0a6ea849d89633ad76aa
Gordi42 <silvano98@hotmail.de>
2024-12-01 10:57:10 +0100

    fix missing default shared axes in domain decomposition

--- src/fridom/framework/domain_decomposition/domain_decomposition.py
+++ src/fridom/framework/domain_decomposition/domain_decomposition.py
@@ -67,7 +67,6 @@ class DomainDecomposition:
         self._n_dims = len(shape)
         self._halo = halo
         self._periods = periods or tuple(True for _ in range(self.n_dims))
-        self._shared_axes = shared_axes
         self._rank = 0
         self._device_ids = device_ids
         self._p_dims = None
