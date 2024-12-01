cbff2fbad47da1bca0c2f459191a35a3b2bb8110
Gordi42 <silvano98@hotmail.de>
2024-12-01 14:16:05 +0100

    remove all the domain decomposition related options from the cartesian grid init method

--- src/fridom/nonhydro/grid/cartesian/grid.py
+++ src/fridom/nonhydro/grid/cartesian/grid.py
@@ -5,16 +5,20 @@ import fridom.framework as fr
 
 @fr.utils.jaxify
 class Grid(fr.grid.cartesian.Grid):
-    def __init__(self, N: list[int], L: list[int],
-                 periodic_bounds: list[bool] = [True, True, True],
-                 decomposition: str = 'slab'):
-        if decomposition == 'slab':
-            shared_axes = [0, 1]
-        elif decomposition == 'pencil':
-            shared_axes = [0]
-        else:
-            raise ValueError(f"Unknown decomposition {decomposition}")
-        super().__init__(N, L, periodic_bounds, shared_axes)
+    def __init__(self, 
+                 N: list[int],
+                 L: list[float],
+                 periodic_bounds: list[bool] | None = None,
+                 domain_decomp: fr.domain_decomposition.DomainDecomposition | None = None,
+                 diff_mod: fr.grid.DiffModule | None = None,
+                 interp_mod: fr.grid.InterpolationModule | None = None
+                 ) -> None:
+        super().__init__(N=N,
+                         L=L,
+                         periodic_bounds=periodic_bounds,
+                         domain_decomp=domain_decomp,
+                         diff_mod=diff_mod,
+                         interp_mod=interp_mod)
 
     def omega(self, 
               k: tuple[float] | tuple[ndarray],
