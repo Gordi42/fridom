2f27a89860c9cfe878b38ad82d29ff91c2ffdc90
Gordi42 <silvano98@hotmail.de>
2024-12-02 16:13:21 +0100

    linting the shallow water barotropic instability experiment

--- examples/shallowwater/barotropic_instability.py
+++ examples/shallowwater/barotropic_instability.py
@@ -12,45 +12,57 @@ import fridom.shallowwater as sw
 #  Experiment settings
 # ----------------------------------------------------------------
 # General settings
-make_video  = True
-fps         = 30
-make_netcdf = False
-exp_name    = "barotropic_instability"
-thumbnail   = f"figures/{exp_name}.png"
+MAKE_VIDEO  = True
+FPS         = 30
+MAKE_NETCDF = False
+EXP_NAME    = "barotropic_instability"
+THUMBNAIL   = f"figures/{EXP_NAME}.png"
 
 # Physical parameters
-rossby_number = 1.0
-burger_number = 1.0 / 100
-f0 = 1.0          # Coriolis parameter
+ROSSBY_NUMBER = 1.0
+BURGER_NUMBER = 1.0 / 100
+F0 = 1.0          # Coriolis parameter
 L = 1.0           # 1 m in x and y (scaled domain)
 
 # specific settings for the barotropic jet
-jet_width = L / 20
-U_jet = f0 * jet_width  # so that the local and global Rossby number are the same
+JET_WIDTH = L / 20
+U_JET = F0 * JET_WIDTH  # so that the local and global Rossby number are the same
 
 # Numerical parameters
-resolution_factor = 9            # 2^9 = 512 grid points
-Nx = 2**(resolution_factor)  # Number of grid points in x and y
+RESOLUTION_FACTOR = 9            # 2^9 = 512 grid points
+NX = 2**(RESOLUTION_FACTOR)  # Number of grid points in x and y
 
 # ----------------------------------------------------------------
 #  Plotting
 # ----------------------------------------------------------------
 class Plotter(sw.modules.animation.ModelPlotter):
+    """Custom plotter for the barotropic instability experiment."""
+    @staticmethod
     def create_figure():
-        import matplotlib.pyplot as plt
+        """Create a figure with a specific size and resolution."""
+        import matplotlib.pyplot as plt  # pylint: disable=import-outside-toplevel
         return plt.figure(figsize=(6, 4.5), dpi=256, tight_layout=True)
 
+    @staticmethod
     def prepare_arguments(mz: sw.ModelState) -> dict:
+        """Prepare the arguments for the plot function."""
         # skip every 4th point for the quiver plot
         skip = 2**(9-5)
-        return {"z": mz.z.xrs[::skip,::skip],
-                "pot_vort": mz.z.pot_vort.xr,
-                "t": mz.time}
+        state = mz.z.xrs[::skip,::skip]
+        pot_vort = mz.z.pot_vort.xr
+        return {"state": state, "pot_vort": pot_vort, "t": mz.time}
 
-    def update_figure(fig, z, pot_vort, t) -> None:
+    @staticmethod
+    def update_figure(fig, *args, **kwargs):
+        """Plot the fields on the figure."""
+        # get the keyword arguments
+        state = kwargs["state"]
+        pot_vort = kwargs["pot_vort"]
+        t = kwargs["t"]
+        # plot the potential vorticity and the velocity field
         ax = fig.add_subplot(111)
         pot_vort.plot(ax=ax, cmap="RdBu_r", vmax=250, vmin=-50, extend='both')
-        key = z.plot.quiver("x", "y", "u", "v", scale=1.5, add_guide=False)
+        key = state.plot.quiver("x", "y", "u", "v", scale=1.5, add_guide=False)
         label_velo = 0.05
         ax.quiverkey(key, X=0.9, Y=1.05, U=label_velo,
                     label=f'{label_velo} [m/s]', labelpos='E')
@@ -62,40 +74,41 @@ class Plotter(sw.modules.animation.ModelPlotter):
 # ----------------------------------------------------------------
 @sw.utils.skip_on_doc_build
 def main():
+    """Run the barotropic instability experiment."""
     # ----------------------------------------------------------------
     #  Create the grid and model settings
     # ----------------------------------------------------------------
-    grid = sw.grid.cartesian.Grid(N=(Nx,Nx), L=(L,L))
-    mset = sw.ModelSettings(grid=grid, 
-                            f0=f0,
-                            Ro=rossby_number, 
-                            csqr=burger_number)
-    mset.time_stepper.dt = 2 / Nx
+    grid = sw.grid.cartesian.Grid(N=(NX,NX), L=(L,L))
+    mset = sw.ModelSettings(grid=grid,
+                            f0=F0,
+                            Ro=ROSSBY_NUMBER,
+                            csqr=BURGER_NUMBER)
+    mset.time_stepper.dt = 2 / NX
 
     # ----------------------------------------------------------------
     #  Add custom modules to the model settings
     # ----------------------------------------------------------------
     # add a video writer
-    if make_video:
+    if MAKE_VIDEO:
         mset.diagnostics.add_module(sw.modules.animation.VideoWriter(
             Plotter,
             model_time_per_second=20.0,
-            filename=exp_name, fps=fps))
+            filename=EXP_NAME, fps=FPS))
 
     # create a NetCDF writer to save the output
-    if make_netcdf:
+    if MAKE_NETCDF:
         mset.diagnostics.add_module(sw.modules.NetCDFWriter(
             get_variables = lambda mz: mz.z.field_list + [mz.z.pot_vort],
             write_interval = 1.0,
-            filename=exp_name))
+            filename=EXP_NAME))
 
     # create a thumbnail saver
     mset.diagnostics.add_module(sw.modules.FigureSaver(
-        filename=thumbnail, model_time=40, plotter=Plotter))
-    
+        filename=THUMBNAIL, model_time=40, plotter=Plotter))
+
     # biharmonic friction as a simple way to dissipate energy at the smallest scales
-    dx = L/Nx
-    viscosity = 0.01 * U_jet * rossby_number * dx**3
+    dx = L/NX
+    viscosity = 0.01 * U_JET * ROSSBY_NUMBER * dx**3
     friction = sw.modules.closures.BiharmonicFriction(ah=viscosity)
     mset.tendencies.add_module(friction)
 
@@ -104,9 +117,9 @@ def main():
     # ----------------------------------------------------------------
     #  Create the initial condition
     # ----------------------------------------------------------------
-    z = U_jet * sw.initial_conditions.Jet(
-        mset, 
-        width=jet_width, 
+    z = U_JET * sw.initial_conditions.Jet(
+        mset,
+        width=JET_WIDTH,
         wavenum=2,           # wavenumber of the perturbation
         pos=0.5,             # jet is in the middle
         waveamp=1e-2)
