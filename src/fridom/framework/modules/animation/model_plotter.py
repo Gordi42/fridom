a440575674d7f5e4b7ce9e03ca232944c6c8d6d2
Gordi42 <silvano98@hotmail.de>
2024-12-02 16:12:51 +0100

    linting the model plotter

--- src/fridom/framework/modules/animation/model_plotter.py
+++ src/fridom/framework/modules/animation/model_plotter.py
@@ -1,8 +1,6 @@
-# Import external modules
-from typing import TYPE_CHECKING
-# Import type information
-if TYPE_CHECKING:
-    from fridom.framework.model_state import ModelState
+"""model_plotter.py - A module for creating and updating a figure object"""
+import numpy as np
+import fridom.framework as fr
 
 class ModelPlotter:
     """
@@ -29,37 +27,47 @@ class ModelPlotter:
         this method does not need to be overwritten. However, if a different
         plotting library is used, this method must be overwritten.
     """
-    def __new__(cls, mz: 'ModelState'):
+    def __new__(cls, mz: 'fr.ModelState'):
         fig = cls.create_figure()
         cls.update_figure(fig, **cls.prepare_arguments(mz))
         return fig
 
+    @staticmethod
     def create_figure():
         """
         This method should create a figure object 
         (e.g. matplotlib figure) and return it.
         """
-        import matplotlib.pyplot as plt
+        import matplotlib.pyplot as plt  # pylint: disable=import-outside-toplevel
         return plt.figure()
 
-    def prepare_arguments(mz: 'ModelState') -> dict:
+    @staticmethod
+    def prepare_arguments(mz: 'fr.ModelState') -> dict:
         """
         This method should prepare the arguments for the update_figure method.
         """
         raise NotImplementedError
 
-    def update_figure(fig, **kwargs) -> None:
+    @staticmethod
+    def update_figure(fig, *args, **kwargs) -> None:
         """
         This method should update the figure object with the
         given model state.
         """
         raise NotImplementedError
 
+    @staticmethod
     def convert_to_img(fig):
         """
         This method should convert the figure object to a numpy image array.
         """
-        import numpy as np
+        # first we draw the figure
         fig.canvas.draw()
-        img = np.array(fig.canvas.renderer._renderer)
+        # access the renderer
+        renderer = fig.canvas.get_renderer()
+        # get the rgba buffer from the renderer
+        rgba_buffer = renderer.buffer_rgba()
+        # convert the rgba buffer to a numpy array
+        img = np.array(rgba_buffer)
+        # return the image
         return img
