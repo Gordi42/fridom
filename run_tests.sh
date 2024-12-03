3df5aa70d53f2208d3c3d7c6e929bfe26a408ef9
Gordi42 <silvano98@hotmail.de>
2024-12-03 16:32:22 +0100

    the run_tests script should now return an error if some test failed

--- run_tests.sh
+++ run_tests.sh
@@ -37,12 +37,21 @@ echo "Running tests in $TARGET with backends: ${BACKENDS[@]}"
 # first erase old coverage data
 coverage erase
 
+# Initialize an error flag
+ERROR=0
+
 # Run tests for each backend
 for backend in "${BACKENDS[@]}";
 do
     echo "Running tests for backend: $backend"
-    FRIDOM_BACKEND=$backend coverage run -a -m pytest $TARGET
+    FRIDOM_BACKEND=$backend coverage run -a -m pytest $TARGET || ERROR=1
 done
 
 # Report coverage
 coverage report -m
+
+# Exit with error if any test failed
+if [ $ERROR_FLAG -ne 0 ]; then
+    echo "Some tests failed."
+    exit 1
+fi
