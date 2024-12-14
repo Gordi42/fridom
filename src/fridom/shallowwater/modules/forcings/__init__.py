667da543ba22ada02a65c0bf00810d228c53ead5
Gordi42 <silvano98@hotmail.de>
2024-12-14 18:24:22 +0100

    test the imports for the shallowwater forcings

--- src/fridom/shallowwater/modules/forcings/__init__.py
+++ src/fridom/shallowwater/modules/forcings/__init__.py
@@ -1,14 +1,22 @@
-"""
-# Forcings
-
-This module contains classes that generate forcing terms for the shallow water model.
-
-## Classes:
-    - GaussianWaveMaker: forces the u-component of the velocity field.
-    - PolarizedWaveMaker: A polarized wave maker that creates a wave package.
-"""
+"""Forcing modules for the shallow water model."""
 from typing import TYPE_CHECKING
 
-if TYPE_CHECKING:
+from lazypimp import setup
+
+# ================================================================
+#  Disable lazy loading for type checking
+# ================================================================
+
+if TYPE_CHECKING:  # pragma: no cover
     from .gaussian_wave_maker import GaussianWaveMaker
     from .polarized_wave_maker import PolarizedWaveMaker
+
+# ================================================================
+#  Setup lazy loading
+# ================================================================
+
+all_modules_by_origin = {}
+
+all_imports_by_origin = {}
+
+setup(__name__, all_modules_by_origin, all_imports_by_origin)
