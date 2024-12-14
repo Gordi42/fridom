9629cfd975be3f7315d84d01eb7f0874ccf8f3df
Gordi42 <silvano98@hotmail.de>
2024-12-14 18:10:47 +0100

    test the imports of the shallow water initial conditions

--- src/fridom/shallowwater/initial_conditions/__init__.py
+++ src/fridom/shallowwater/initial_conditions/__init__.py
@@ -1,17 +1,16 @@
-"""
-Initial Conditions for the shallow water model
-"""
-from lazypimp import setup
+"""Initial Conditions for the shallow water model."""
 from typing import TYPE_CHECKING
+
+from lazypimp import setup
 
 # ================================================================
 #  Disable lazy loading for type checking
 # ================================================================
-if TYPE_CHECKING:
+if TYPE_CHECKING:  # pragma: no cover
+    from .coherent_eddy import CoherentEddy
     from .jet import Jet
     from .single_wave import SingleWave
-    from .coherent_eddy import CoherentEddy
-    
+
 # ================================================================
 #  Setup lazy loading
 # ================================================================
@@ -19,7 +18,7 @@ base_path = "fridom.shallowwater.initial_conditions"
 
 all_modules_by_origin = { }
 
-all_imports_by_origin = { 
+all_imports_by_origin = {
     f"{base_path}.jet": ["Jet"],
     f"{base_path}.coherent_eddy": ["CoherentEddy"],
     f"{base_path}.single_wave": ["SingleWave"],
