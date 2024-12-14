5870cb8c915aeb68b4da71c7a047c9f114fa9804
Gordi42 <silvano98@hotmail.de>
2024-12-14 17:05:41 +0100

    test the imports of the animation module

--- src/fridom/framework/modules/animation/__init__.py
+++ src/fridom/framework/modules/animation/__init__.py
@@ -1,18 +1,16 @@
-"""
-Modules for creating animated output of the model
-"""
-from lazypimp import setup
+"""Modules for creating animated output of the model."""
 from typing import TYPE_CHECKING
+
+from lazypimp import setup
 
 # ================================================================
 #  Disable lazy loading for type checking
 # ================================================================
-if TYPE_CHECKING:
-    # import classes
-    from .model_plotter import ModelPlotter
+if TYPE_CHECKING:  # pragma: no cover
     from .live_animation import LiveAnimation
+    from .model_plotter import ModelPlotter
     from .video_writer import VideoWriter
-    
+
 # ================================================================
 #  Setup lazy loading
 # ================================================================
@@ -20,10 +18,10 @@ base_path = "fridom.framework.modules.animation"
 
 all_modules_by_origin = { }
 
-all_imports_by_origin = { 
+all_imports_by_origin = {
     f"{base_path}.model_plotter": ["ModelPlotter"],
     f"{base_path}.live_animation": ["LiveAnimation"],
-    f"{base_path}.video_writer": ["VideoWriter"], 
+    f"{base_path}.video_writer": ["VideoWriter"],
 }
 
 setup(__name__, all_modules_by_origin, all_imports_by_origin)
