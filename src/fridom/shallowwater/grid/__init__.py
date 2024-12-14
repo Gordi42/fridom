415c93f283b8e262aad6852938171e29fbe749a4
Gordi42 <silvano98@hotmail.de>
2024-12-14 18:07:00 +0100

    test the imports of the shallowwater grid

--- src/fridom/shallowwater/grid/__init__.py
+++ src/fridom/shallowwater/grid/__init__.py
@@ -1,18 +1,15 @@
-"""
-Grids for the shallow water model.
-"""
+"""Grids for the shallow water model."""
 from typing import TYPE_CHECKING
+
 from lazypimp import setup
 
 # ================================================================
 #  Disable lazy loading for type checking
 # ================================================================
-if TYPE_CHECKING:
-    # import modules
-    from . import cartesian
+if TYPE_CHECKING:  # pragma: no cover
+    from fridom.framework.grid import AxisPosition, BCType, Position
 
-    # import classes
-    from fridom.framework.grid import AxisPosition, Position, BCType
+    from . import cartesian
 
 # ================================================================
 #  Setup lazy loading
@@ -22,7 +19,7 @@ base = "fridom.shallowwater.grid"
 all_modules_by_origin = { base: ["cartesian"] }
 
 all_imports_by_origin = {
-    "fridom.framework.grid": ["AxisPosition", "Position", "BCType"]
+    "fridom.framework.grid": ["AxisPosition", "Position", "BCType"],
 }
 
 setup(__name__, all_modules_by_origin, all_imports_by_origin)
