6f49512b38a31ce356fd14f1d078551ac021232e
Gordi42 <silvano98@hotmail.de>
2024-12-14 16:55:57 +0100

    test imports of the cartesian grid

--- src/fridom/framework/grid/cartesian/__init__.py
+++ src/fridom/framework/grid/cartesian/__init__.py
@@ -1,21 +1,23 @@
-from lazypimp import setup
+"""Base module for Cartesian grids."""
 from typing import TYPE_CHECKING
+
+from lazypimp import setup
 
 # ================================================================
 #  Disable lazy loading for type checking
 # ================================================================
-if TYPE_CHECKING:
-    # import modules
+if TYPE_CHECKING:  # pragma: no cover
+    # import all modules
     from . import discrete_spectral_operators
-
-    # import classes
-    from .grid import Grid
     from .fft import FFT
     from .finite_differences import FiniteDifferences
-    from .spectral_diff import SpectralDiff
+
+    # import all classes
+    from .grid import Grid
     from .linear_interpolation import LinearInterpolation
     from .polynomial_interpolation import PolynomialInterpolation
-    
+    from .spectral_diff import SpectralDiff
+
 # ================================================================
 #  Setup lazy loading
 # ================================================================
@@ -25,7 +27,7 @@ all_modules_by_origin = {
     base: ["discrete_spectral_operators"],
 }
 
-all_imports_by_origin = { 
+all_imports_by_origin = {
     f"{base}.grid": ["Grid"],
     f"{base}.fft": ["FFT"],
     f"{base}.finite_differences": ["FiniteDifferences"],
