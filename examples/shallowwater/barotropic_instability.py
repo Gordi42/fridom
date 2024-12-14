b80a64aba1fa5b65f141038aa860e7a9157ee729
Gordi42 <silvano98@hotmail.de>
2024-12-14 18:55:47 +0100

    don't use capitalized constants in the examples

--- examples/shallowwater/barotropic_instability.py
+++ examples/shallowwater/barotropic_instability.py
@@ -1,46 +1,52 @@
 r"""
-Barotropic Instability
-======================
+Barotropic Instability.
+
+=======================
 
 An instable barotropic jet with a perturbation on top of it.
 
 .. video:: videos/barotropic_instability.mp4
 """
+from __future__ import annotations
+
+import matplotlib.pyplot as plt
+
 import fridom.shallowwater as sw
 
 # ----------------------------------------------------------------
 #  Experiment settings
 # ----------------------------------------------------------------
 # General settings
-MAKE_VIDEO  = True
-FPS         = 30
-MAKE_NETCDF = False
-EXP_NAME    = "barotropic_instability"
-THUMBNAIL   = f"figures/{EXP_NAME}.png"
+make_video  = True
+fps         = 30
+make_netcdf = False
+exp_name    = "barotropic_instability"
+thumbnail   = f"figures/{exp_name}.png"
 
 # Physical parameters
-ROSSBY_NUMBER = 1.0
-BURGER_NUMBER = 1.0 / 100
-F0 = 1.0          # Coriolis parameter
+rossby_number = 1.0
+burger_number = 1.0 / 100
+f0 = 1.0          # Coriolis parameter
 L = 1.0           # 1 m in x and y (scaled domain)
 
 # specific settings for the barotropic jet
-JET_WIDTH = L / 20
-U_JET = F0 * JET_WIDTH  # so that the local and global Rossby number are the same
+jet_width = L / 20
+u_jet = f0 * jet_width  # so that the local and global Rossby number are the same
 
 # Numerical parameters
-RESOLUTION_FACTOR = 9            # 2^9 = 512 grid points
-NX = 2**(RESOLUTION_FACTOR)  # Number of grid points in x and y
+resolution_factor = 9            # 2^9 = 512 grid points
+nx = 2**(resolution_factor)  # Number of grid points in x and y
 
 # ----------------------------------------------------------------
 #  Plotting
 # ----------------------------------------------------------------
 class Plotter(sw.modules.animation.ModelPlotter):
+
     """Custom plotter for the barotropic instability experiment."""
+
     @staticmethod
-    def create_figure():
+    def create_figure() -> plt.Figure:
         """Create a figure with a specific size and resolution."""
-        import matplotlib.pyplot as plt  # pylint: disable=import-outside-toplevel
         return plt.figure(figsize=(6, 4.5), dpi=256, tight_layout=True)
 
     @staticmethod
@@ -53,7 +59,7 @@ class Plotter(sw.modules.animation.ModelPlotter):
         return {"state": state, "pot_vort": pot_vort, "t": mz.clock.time}
 
     @staticmethod
-    def update_figure(fig, *args, **kwargs):
+    def update_figure(fig: plt.Figure, *_args: any, **kwargs: any) -> None:
         """Plot the fields on the figure."""
         # get the keyword arguments
         state = kwargs["state"]
@@ -61,54 +67,54 @@ class Plotter(sw.modules.animation.ModelPlotter):
         t = kwargs["t"]
         # plot the potential vorticity and the velocity field
         ax = fig.add_subplot(111)
-        pot_vort.plot(ax=ax, cmap="RdBu_r", vmax=250, vmin=-50, extend='both')
+        pot_vort.plot(ax=ax, cmap="RdBu_r", vmax=250, vmin=-50, extend="both")
         key = state.plot.quiver("x", "y", "u", "v", scale=1.5, add_guide=False)
         label_velo = 0.05
         ax.quiverkey(key, X=0.9, Y=1.05, U=label_velo,
-                    label=f'{label_velo} [m/s]', labelpos='E')
-        ax.set_aspect('equal')
-        ax.set_title(f't={t:.0f}s', fontsize=18)
+                    label=f"{label_velo} [m/s]", labelpos="E")
+        ax.set_aspect("equal")
+        ax.set_title(f"t={t:.0f}s", fontsize=18)
 
 # ----------------------------------------------------------------
 #  The main model
 # ----------------------------------------------------------------
 @sw.utils.skip_on_doc_build
-def main():
+def main() -> None:
     """Run the barotropic instability experiment."""
     # ----------------------------------------------------------------
     #  Create the grid and model settings
     # ----------------------------------------------------------------
-    grid = sw.grid.cartesian.Grid(N=(NX,NX), L=(L,L))
+    grid = sw.grid.cartesian.Grid(N=(nx,nx), L=(L,L))
     mset = sw.ModelSettings(grid=grid,
-                            f0=F0,
-                            Ro=ROSSBY_NUMBER,
-                            csqr=BURGER_NUMBER)
-    mset.time_stepper.dt = 2 / NX
+                            f0=f0,
+                            Ro=rossby_number,
+                            csqr=burger_number)
+    mset.time_stepper.dt = 2 / nx
 
     # ----------------------------------------------------------------
     #  Add custom modules to the model settings
     # ----------------------------------------------------------------
     # add a video writer
-    if MAKE_VIDEO:
+    if make_video:
         mset.diagnostics.add_module(sw.modules.animation.VideoWriter(
             Plotter,
             model_time_per_second=20.0,
-            filename=EXP_NAME, fps=FPS))
+            filename=exp_name, fps=fps))
 
     # create a NetCDF writer to save the output
-    if MAKE_NETCDF:
+    if make_netcdf:
         mset.diagnostics.add_module(sw.modules.NetCDFWriter(
             get_variables = lambda mz: [*mz.z.field_list, mz.z.pot_vort],
             write_trigger = sw.ClockTrigger(time_interval=1.0),
-            filename=EXP_NAME))
+            filename=exp_name))
 
     # create a thumbnail saver
     mset.diagnostics.add_module(sw.modules.FigureSaver(
-        filename=THUMBNAIL, model_time=40, plotter=Plotter))
+        filename=thumbnail, model_time=40, plotter=Plotter))
 
     # biharmonic friction as a simple way to dissipate energy at the smallest scales
-    dx = L/NX
-    viscosity = 0.01 * U_JET * ROSSBY_NUMBER * dx**3
+    dx = L/nx
+    viscosity = 0.01 * u_jet * rossby_number * dx**3
     friction = sw.modules.closures.BiharmonicFriction(ah=viscosity)
     mset.tendencies.add_module(friction)
 
@@ -117,9 +123,9 @@ def main():
     # ----------------------------------------------------------------
     #  Create the initial condition
     # ----------------------------------------------------------------
-    z = U_JET * sw.initial_conditions.Jet(
+    z = u_jet * sw.initial_conditions.Jet(
         mset,
-        width=JET_WIDTH,
+        width=jet_width,
         wavenum=2,           # wavenumber of the perturbation
         pos=0.5,             # jet is in the middle
         waveamp=1e-2)
