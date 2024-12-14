4db42c15633f2f85dc8b7659446cdd83787edebe
Gordi42 <silvano98@hotmail.de>
2024-12-14 16:40:40 +0100

    test utils init

--- src/fridom/framework/utils/__init__.py
+++ src/fridom/framework/utils/__init__.py
@@ -1,5 +1,7 @@
 """Utility functions and classes for the FRIDOM framework."""
+from contextlib import suppress
 from typing import TYPE_CHECKING
+
 from lazypimp import setup
 
 # for the sphinx autodoc to work, we must import jaxify here in non-lazy mode
@@ -9,25 +11,21 @@ from lazypimp import setup
 # This is not a problem because the exploration import only needs the
 # all_modules_by_origin and all_imports_by_origin dictionaries.
 # So we can safely ignore the ImportError here.
-try:
+with suppress(ImportError):
     from .jax_utils import jaxify
-except ImportError:
-    # We are in the module exploration phase of doc building
-    pass
 
 # ================================================================
 #  Disable lazy loading for type checking
 # ================================================================
-if TYPE_CHECKING:
-    # import content
-    from .mpi import I_AM_MAIN_RANK, MPI_AVAILABLE, mpi_barrier, get_mpi_size
-    from .printing import print_bar, print_job_init_info
-    from .formatting import humanize_length, humanize_time, humanize_number
-    from .filesystem import chdir_to_submit_dir, stdout_is_file
+if TYPE_CHECKING:  # pragma: no cover
     from .array_ops import SliceableAttribute, modify_array, random_array
+    from .decorators import cache_figure, skip_on_doc_build
+    from .filesystem import chdir_to_submit_dir, stdout_is_file
+    from .formatting import humanize_length, humanize_number, humanize_time
+    from .jax_utils import free_memory, inspect_jitted_function, jaxify, jaxjit
+    from .mpi import I_AM_MAIN_RANK, MPI_AVAILABLE, get_mpi_size, mpi_barrier
     from .numpy_utils import to_numpy, to_seconds
-    from .jax_utils import jaxjit, jaxify, inspect_jitted_function, free_memory
-    from .decorators import skip_on_doc_build, cache_figure
+    from .printing import print_bar, print_job_init_info
 
 # ================================================================
 #  Setup lazy loading
@@ -44,7 +42,7 @@ all_imports_by_origin = {
     f"{BASE}.array_ops": ["SliceableAttribute", "modify_array", "random_array"],
     f"{BASE}.numpy_utils": ["to_numpy", "to_seconds"],
     f"{BASE}.jax_utils": [
-        "jaxjit", "jaxify", "inspect_jitted_function", "free_memory"
+        "jaxjit", "jaxify", "inspect_jitted_function", "free_memory",
         ],
     f"{BASE}.decorators": ["skip_on_doc_build", "cache_figure"],
 }
