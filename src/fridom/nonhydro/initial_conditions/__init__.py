d1939813ef946230a87ac82b1f2ff64dea959576
Gordi42 <silvano98@hotmail.de>
2024-12-14 17:24:51 +0100

    test the imports of nonhydro initial conditions

--- src/fridom/nonhydro/initial_conditions/__init__.py
+++ src/fridom/nonhydro/initial_conditions/__init__.py
@@ -1,22 +1,23 @@
-"""
-Initial Conditions for the Nonhydrostatic Model
-"""
-from lazypimp import setup
+"""Initial Conditions for the Nonhydrostatic Model."""
 from typing import TYPE_CHECKING
+
+from lazypimp import setup
 
 # ================================================================
 #  Disable lazy loading for type checking
 # ================================================================
 if TYPE_CHECKING:
-    from .single_wave import SingleWave
-    from .kelvin_wave import KelvinWave
-    from .wave_package import WavePackage
     from .barotropic_jet import BarotropicJet
-    from .jet import Jet
     from .coherent_eddy import CoherentEddy
-    from .geostrophic_spectra import geostrophic_energy_spectrum, \
-                                     RandomGeostrophicSpectra
-    
+    from .geostrophic_spectra import (
+        RandomGeostrophicSpectra,
+        geostrophic_energy_spectrum,
+    )
+    from .jet import Jet
+    from .kelvin_wave import KelvinWave
+    from .single_wave import SingleWave
+    from .wave_package import WavePackage
+
 # ================================================================
 #  Setup lazy loading
 # ================================================================
@@ -24,14 +25,14 @@ base_path = "fridom.nonhydro.initial_conditions"
 
 all_modules_by_origin = { }
 
-all_imports_by_origin = { 
+all_imports_by_origin = {
     f"{base_path}.single_wave": ["SingleWave"],
     f"{base_path}.kelvin_wave": ["KelvinWave"],
     f"{base_path}.wave_package": ["WavePackage"],
     f"{base_path}.barotropic_jet": ["BarotropicJet"],
     f"{base_path}.jet": ["Jet"],
     f"{base_path}.coherent_eddy": ["CoherentEddy"],
-    f"{base_path}.geostrophic_spectra": ["geostrophic_energy_spectrum", 
+    f"{base_path}.geostrophic_spectra": ["geostrophic_energy_spectrum",
                                          "RandomGeostrophicSpectra"],
 }
 
