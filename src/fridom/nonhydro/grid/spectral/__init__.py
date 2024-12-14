94f0c1bf67fe8ebc26cb439ed347fcf7018fcc8a
Gordi42 <silvano98@hotmail.de>
2024-12-14 17:23:04 +0100

    test imports of the spectral grid in the nonhydro model

--- src/fridom/nonhydro/grid/spectral/__init__.py
+++ src/fridom/nonhydro/grid/spectral/__init__.py
@@ -1,15 +1,14 @@
-from lazypimp import setup
+"""Spectral grid module for the nonhydrostatic model."""
 from typing import TYPE_CHECKING
+
+from lazypimp import setup
 
 # ================================================================
 #  Disable lazy loading for type checking
 # ================================================================
 if TYPE_CHECKING:
-    # import modules
-
-    # import classes
     from .grid import Grid
-    
+
 # ================================================================
 #  Setup lazy loading
 # ================================================================
@@ -17,7 +16,7 @@ base_nh = "fridom.nonhydro.grid.spectral"
 
 all_modules_by_origin = {}
 
-all_imports_by_origin = { 
+all_imports_by_origin = {
     f"{base_nh}.grid": ["Grid"],
 }
 
