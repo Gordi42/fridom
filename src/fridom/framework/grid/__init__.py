a4be4012c2ba9528e46010f6b47ccbd782151370
Gordi42 <silvano98@hotmail.de>
2024-12-14 16:43:05 +0100

    test the grid imports

--- src/fridom/framework/grid/__init__.py
+++ src/fridom/framework/grid/__init__.py
@@ -1,36 +1,34 @@
-"""
-All grid related classes and functions.
-"""
-from lazypimp import setup
+"""All grid related classes and functions."""
 from typing import TYPE_CHECKING
+
+from lazypimp import setup
 
 # ================================================================
 #  Disable lazy loading for type checking
 # ================================================================
-if TYPE_CHECKING:
-    # import modules
-    from . import cartesian
-    from . import spectral
-
-    # import classes
-    from .grid_base import GridBase
+if TYPE_CHECKING:  # pragma: no cover
+    # import all modules
+    from . import cartesian, spectral
+    from .boundary_type import BCType
     from .diff_module import DiffModule
-    from .interpolation_module import InterpolationModule
     from .dummy_interpolation import DummyInterpolation
+    from .fft_padding import FFTPadding
+
+    # import all classes
+    from .grid_base import GridBase
+    from .interpolation_module import InterpolationModule
     from .position import AxisPosition, Position
     from .water_mask import WaterMask
-    from .boundary_type import BCType
-    from .fft_padding import FFTPadding
-    
+
 # ================================================================
 #  Setup lazy loading
 # ================================================================
 base = "fridom.framework.grid"
 
-all_modules_by_origin = { base: ["cartesian", 
+all_modules_by_origin = { base: ["cartesian",
                                  "spectral"] }
 
-all_imports_by_origin = { 
+all_imports_by_origin = {
     f"{base}.grid_base": ["GridBase"],
     f"{base}.diff_module": ["DiffModule"],
     f"{base}.interpolation_module": ["InterpolationModule"],
