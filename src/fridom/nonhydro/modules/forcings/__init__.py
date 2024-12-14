da66ed4d67c7cb16a33056996c4297b0536bda34
Gordi42 <silvano98@hotmail.de>
2024-12-14 17:33:53 +0100

    test the imports of the nonhydro forcings

--- src/fridom/nonhydro/modules/forcings/__init__.py
+++ src/fridom/nonhydro/modules/forcings/__init__.py
@@ -1,17 +1,16 @@
-"""
-This module contains classes that generate forcing terms for the nonhydrostatic model.
-"""
-from lazypimp import setup
+"""Forcing modules for the nonhydrostatic model."""
 from typing import TYPE_CHECKING
+
+from lazypimp import setup
 
 # ================================================================
 #  Disable lazy loading for type checking
 # ================================================================
-if TYPE_CHECKING:
+if TYPE_CHECKING:  # pragma: no cover
+    from fridom.framework.modules.forcings import Relaxation
+
     from .gaussian_wave_maker import GaussianWaveMaker
     from .polarized_wave_maker import PolarizedWaveMaker
-
-    from fridom.framework.modules.forcings import Relaxation
 
 # ================================================================
 #  Setup lazy loading
@@ -21,7 +20,7 @@ fr_base_path = "fridom.framework.modules.forcings"
 
 all_modules_by_origin = { }
 
-all_imports_by_origin = { 
+all_imports_by_origin = {
     f"{base_path}.gaussian_wave_maker": ["GaussianWaveMaker"],
     f"{base_path}.polarized_wave_maker": ["PolarizedWaveMaker"],
     f"{fr_base_path}.relaxation": ["Relaxation"],
