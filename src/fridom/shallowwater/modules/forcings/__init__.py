336e54716e3a5ac715918985a58b7eb064c0a91d
Gordi42 <silvano98@hotmail.de>
2024-12-14 14:59:18 +0100

    remove shallow water model forcing imports for now

--- src/fridom/shallowwater/modules/forcings/__init__.py
+++ src/fridom/shallowwater/modules/forcings/__init__.py
@@ -7,6 +7,8 @@ This module contains classes that generate forcing terms for the shallow water m
     - GaussianWaveMaker: forces the u-component of the velocity field.
     - PolarizedWaveMaker: A polarized wave maker that creates a wave package.
 """
+from typing import TYPE_CHECKING
 
-from .gaussian_wave_maker import GaussianWaveMaker
-from .polarized_wave_maker import PolarizedWaveMaker
+if TYPE_CHECKING:
+    from .gaussian_wave_maker import GaussianWaveMaker
+    from .polarized_wave_maker import PolarizedWaveMaker
