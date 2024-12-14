10d3fcf1b51c67305554c9ed4a1966b37b9ac937
Gordi42 <silvano98@hotmail.de>
2024-12-14 17:18:31 +0100

    test the imports in the nonhydro/grid

--- src/fridom/nonhydro/grid/__init__.py
+++ src/fridom/nonhydro/grid/__init__.py
@@ -1,19 +1,15 @@
-"""
-Module that stores all available grids for the non-hydrostatic model.
-"""
+"""Module that stores all available grids for the non-hydrostatic model."""
 from typing import TYPE_CHECKING
+
 from lazypimp import setup
 
 # ================================================================
 #  Disable lazy loading for type checking
 # ================================================================
-if TYPE_CHECKING:
-    # import modules
-    from . import cartesian
-    from . import spectral
+if TYPE_CHECKING:  # pragma: no cover
+    from fridom.framework.grid import AxisPosition, BCType, Position
 
-    # import classes
-    from fridom.framework.grid import AxisPosition, Position, BCType
+    from . import cartesian, spectral
 
 # ================================================================
 #  Setup lazy loading
@@ -24,7 +20,7 @@ all_modules_by_origin = { base: ["cartesian",
                                  "spectral"] }
 
 all_imports_by_origin = {
-    "fridom.framework.grid": ["AxisPosition", "Position", "BCType"]
+    "fridom.framework.grid": ["AxisPosition", "Position", "BCType"],
 }
 
 setup(__name__, all_modules_by_origin, all_imports_by_origin)
