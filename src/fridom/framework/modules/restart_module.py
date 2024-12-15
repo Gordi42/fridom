1dd9902a4c2264b2fe01b881ffbb395977d73294
Gordi42 <silvano98@hotmail.de>
2024-12-15 01:35:38 +0100

    move restart method into restart module

--- src/fridom/framework/modules/restart_module.py
+++ src/fridom/framework/modules/restart_module.py
@@ -1,14 +1,20 @@
-import fridom.framework as fr
+"""Restart module for cluster computing."""
+from __future__ import annotations
+
 import os
+import sys
 import time
+
 import numpy as np
+
+import fridom.framework as fr
 
 
 class RestartModule(fr.modules.Module):
     name = "Restart Module"
     def __init__(self,
-                 realtime_interval: 'np.timedelta64 | None' = None,
-                 modeltime_interval: 'np.timedelta64 | None' = None,
+                 realtime_interval: np.timedelta64 | None = None,
+                 modeltime_interval: np.timedelta64 | None = None,
                  iteration_interval: int | None = None,
                  restart_command: str | None = None,
                  filename: str = "model",
@@ -47,7 +53,7 @@ class RestartModule(fr.modules.Module):
         #  Set the restart command
         # ----------------------------------------------------------------
         if restart_command is None and self.is_enabled == True:
-            job_id = os.getenv('SLURM_JOB_ID')
+            job_id = os.getenv("SLURM_JOB_ID")
             if job_id is None:
                 fr.log.warning(
                     "No restart command is set. The model will not be able to restart.")
@@ -55,13 +61,13 @@ class RestartModule(fr.modules.Module):
             else:
                 import subprocess
                 job_info = subprocess.run(
-                        ['scontrol', 'show', 'job', job_id],
+                        ["scontrol", "show", "job", job_id],
                         capture_output=True,
-                        text=True)
+                        text=True, check=False)
                 command = None
-                for line in job_info.stdout.split('\n'):
-                    if line.strip().startswith('Command='):
-                        command = line.split('=', 1)[1].strip()
+                for line in job_info.stdout.split("\n"):
+                    if line.strip().startswith("Command="):
+                        command = line.split("=", 1)[1].strip()
                 if command is None:
                     fr.log.warning(
                         "No restart command is set. The model will not be able to restart.")
@@ -83,20 +89,19 @@ class RestartModule(fr.modules.Module):
         self._last_restart_iteration = None
 
     @fr.modules.module_method
-    def setup(self, mset: 'fr.ModelSettingsBase') -> None:
+    def setup(self, mset: fr.ModelSettingsBase) -> None:
         super().setup(mset)
         fr.log.verbose("Touching the restart directory.")
         os.makedirs(self.directory, exist_ok=True)
-        return
 
     @fr.modules.module_method
-    def should_restart(self, mz: 'fr.ModelState') -> bool:
+    def should_restart(self, mz: fr.ModelState) -> bool:
         # ----------------------------------------------------------------
         #  Realtime interval
         # ----------------------------------------------------------------
         if self.realtime_interval is not None:
             # get elapsed time
-            elapsed_time = np.timedelta64(int(time.time() - fr.config.load_time), 's')
+            elapsed_time = np.timedelta64(int(time.time() - fr.config.load_time), "s")
             if elapsed_time >= self.realtime_interval:
                 fr.log.info(
                     "Realtime restart interval reached. Model will restart.")
@@ -142,15 +147,13 @@ class RestartModule(fr.modules.Module):
             its = [int(f.split("_")[1]) for f in files]
             self.set_full_filename(max(its))
             return True
-        else:
-            fr.log.info("No restart files found. Model will not reload.")
-            return False
+        fr.log.info("No restart files found. Model will not reload.")
+        return False
 
     @fr.modules.module_method
     def reset(self) -> None:
         self._last_restart_modeltime = None
         self._last_restart_iteration = None
-        return
 
     def set_full_filename(self, it: int) -> None:
         if fr.utils.MPI_AVAILABLE:
@@ -159,7 +162,30 @@ class RestartModule(fr.modules.Module):
             rank = 0
         filename = f"{self.filename}_{it}_{rank}.dill"
         self.file = os.path.join(self.directory, filename)
-        return
+
+    def restart(self, model: fr.Model) -> None:
+        fr.log.info(
+            "Stopping model at it: %d, time: %f",
+            model.model_state.clock.it,
+            model.model_state.clock.time)
+        model.stop()
+        model.save(self.file)
+        fr.log.info(model.mset.timer)
+        fr.log.info("Spawning new sbatch job:")
+        fr.log.info(self.restart_command)
+        fr.utils.mpi_barrier()
+        if fr.utils.MPI_AVAILABLE:
+            import subprocess
+            result = subprocess.run(  # noqa: S603
+                self.restart_command.split(),
+                capture_output=True,
+                text=True,
+                check=False)
+            fr.log.notice(result.stdout)
+            if result.stderr:
+                fr.log.error(result.stderr)
+        fr.utils.mpi_barrier()
+        sys.exit()
 
     # ================================================================
     #  Properties
