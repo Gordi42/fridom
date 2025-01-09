456db3f7ae90a2acf8a5de5a67f6bb8e8d67ceb5
Gordi42 <silvano98@hotmail.de>
2025-01-09 17:20:50 +0100

    add tests for exceptions

--- tests/framework/test_exceptions.py
+++ tests/framework/test_exceptions.py
@@ -22,3 +22,48 @@ def test_too_many_arguments_error(max_args, provided_args, raised):
         return
     with pytest.raises(fr.exceptions.TooManyArgumentsError):
         fr.exceptions.TooManyArgumentsError.check(max_args, **provided_args)
+
+@pytest.mark.parametrize(*(
+    "topo, raised",
+    [
+        pytest.param((True, True, True), False, id="full_domain"),
+        pytest.param((True, True, False), True, id="lacking_z"),
+        pytest.param((True, False, True), True, id="lacking_y"),
+        pytest.param((False, True, True), True, id="lacking_x"),
+        pytest.param((False, False, False), True, id="no_domain"),
+
+    ],
+))
+def test_partial_domain_error(topo, raised):
+    grid = fr.grid.cartesian.Grid(N=(3, 3, 3), L=(1, 1, 1))
+    mset = fr.ModelSettingsBase(grid).setup()
+    field = fr.ScalarField(mset, topo=topo)
+    if not raised:
+        fr.exceptions.PartialDomainError.check(field)
+        return
+
+    msg = "Operation not available for fields with topo="
+    with pytest.raises(fr.exceptions.PartialDomainError, match=msg):
+        fr.exceptions.PartialDomainError.check(field)
+
+def test_field_space_error():
+    grid = fr.grid.cartesian.Grid(N=(3, 3, 3), L=(1, 1, 1))
+    mset = fr.ModelSettingsBase(grid).setup()
+    field = fr.ScalarField(mset, is_spectral=True)
+    # Check if the field is in spectral space should pass
+    fr.exceptions.FieldSpaceError.check_if_spectral(field)
+
+    # Check if the field is in physical space should raise an error
+    msg = "Operation not available for fields in spectral space."
+    with pytest.raises(fr.exceptions.FieldSpaceError, match=msg):
+        fr.exceptions.FieldSpaceError.check_if_physical(field)
+
+    # now the same for physical space
+    field = fr.ScalarField(mset, is_spectral=False)
+    # Check if the field is in physical space should pass
+    fr.exceptions.FieldSpaceError.check_if_physical(field)
+
+    # Check if the field is in spectral space should raise an error
+    msg = "Operation not available for fields in physical space."
+    with pytest.raises(fr.exceptions.FieldSpaceError, match=msg):
+        fr.exceptions.FieldSpaceError.check_if_spectral(field)
