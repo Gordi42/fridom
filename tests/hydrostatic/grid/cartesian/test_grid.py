c00868972240a49197f780f8581d3f46be1580d0
Gordi42 <silvano98@hotmail.de>
2025-01-31 18:26:05 +0100

    add tests for the cartesian grid in the hydrostatic model

--- tests/hydrostatic/grid/cartesian/test_grid.py
+++ tests/hydrostatic/grid/cartesian/test_grid.py
@@ -25,6 +25,36 @@ def length(request):
     pytest.param((False, False, False), id="Non-periodic"),
     pytest.param((True, False, True), id="Mixed"),
 ])
+def periodic(request):
+    return request.param
+
+@pytest.fixture(params=[
+    pytest.param((3, 3, 1), id="uneven"),
+    pytest.param((4, 4, 2), id="even"),
+])
+def grid(request):
+    grid = hs.grid.cartesian.Grid(shape = request.param,
+                                  length = [1.5, 2.0, 3.0],
+                                  periodic = [True, True, True])
+    mset = hs.ModelSettings(grid)
+    grid.setup(mset)
+    return grid
+
+@pytest.fixture(params=[
+    "discrete",
+    "continuous",
+])
+def spectral_method(request):
+    return request.param
+
+@pytest.fixture(params=[
+    "0",
+    "1",
+    "-1",
+    "d",
+])
+def mode(request):
+    return request.param
 
 # ================================================================
 #  Tests
@@ -36,8 +66,15 @@ def test_init(shape, length, periodic):
     assert length == grid.L
     assert periodic == grid.periodic_bounds
 
-# TODO(Silvano): Test once we have the model settings
-def test_setup(): ...
+def test_setup(shape, length, periodic):
+    grid = hs.grid.cartesian.Grid(shape, length, periodic)
+    mset = hs.ModelSettings(grid)
+    # check that the grid is not setup initially
+    assert grid.X is None
+    # setup the grid
+    grid.setup(mset)
+    # check if the grid is set up correctly
+    assert grid.X is not None
 
 # TODO(Silvano): Test one example with a model run
 def test_model_run(): ...
@@ -46,11 +83,14 @@ def test_model_run(): ...
 #  Test the spectral analysis methods
 # ----------------------------------------------------------------
 
-# TODO(Silvano): Test once we have the model settings
-def test_omega(): ...
+def test_omega(grid, spectral_method):
+    with pytest.raises(NotImplementedError):
+        grid.omega(kvec=(1.0, 1.0, 1.0), method=spectral_method)
 
-# TODO(Silvano): Test once we have the model settings
-def test_vec_q(): ...
+def test_vec_q(grid, spectral_method, mode):
+    with pytest.raises(NotImplementedError):
+        grid.vec_q(mode=mode, method=spectral_method)
 
-# TODO(Silvano): Test once we have the model settings
-def test_vec_p(): ...
+def test_vec_p(grid, spectral_method, mode):
+    with pytest.raises(NotImplementedError):
+        grid.vec_p(mode=mode, method=spectral_method)
